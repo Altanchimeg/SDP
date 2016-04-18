a1142da34f49593c9118de48e20b519ee962b893
Bayarkhuu Purevgaram <bayarkhuu0512@gmail.com>
2016-04-18 18:03:31 +0900

    View pager added

--- app/src/main/java/com/skytel/sdp/ui/NumberChoiceFragment.java
+++ app/src/main/java/com/skytel/sdp/ui/NumberChoiceFragment.java
@@ -1,10 +1,12 @@
 package com.skytel.sdp.ui;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.*;
+import android.widget.TextView;
 
 import com.skytel.sdp.R;
 
@@ -12,6 +14,13 @@ import com.skytel.sdp.R;
  * Created by bayarkhuu on 4/14/2016.
  */
 public class NumberChoiceFragment extends Fragment {
+    DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+
+    /**
+     * The {@link android.support.v4.view.ViewPager} that will display the object collection.
+     */
+    ViewPager mViewPager;
+
     public NumberChoiceFragment() {
     }
 
@@ -24,6 +33,53 @@ public class NumberChoiceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.main_number_choice, container, false);
+        mDemoCollectionPagerAdapter = new DemoCollectionPagerAdapter(getActivity().getSupportFragmentManager());
+        // Set up the ViewPager, attaching the adapter.
+        mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
+        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+
         return rootView;
+    }
+
+    public static class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
+
+        public DemoCollectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new DemoObjectFragment();
+            Bundle args = new Bundle();
+            args.putInt(DemoObjectFragment.ARG_OBJECT, i + 1); // Our object is just an integer :-P
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            // For this contrived example, we have a 100-object collection.
+            return 100;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "OBJECT " + (position + 1);
+        }
+    }
+
+    public static class DemoObjectFragment extends Fragment {
+
+        public static final String ARG_OBJECT = "object";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_collection_object, container, false);
+            Bundle args = getArguments();
+            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+                    Integer.toString(args.getInt(ARG_OBJECT)));
+            return rootView;
+        }
     }
 }
