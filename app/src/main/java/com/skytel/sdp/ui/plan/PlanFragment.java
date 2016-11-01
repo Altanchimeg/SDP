fc96a300e738b003c24554d6be94c50f4fc94e1d
Bayarkhuu Purevgaram <bayarkhuu0512@gmail.com>
2016-11-01 22:50:53 +0800

    Barchart added

--- app/src/main/java/com/skytel/sdp/ui/plan/PlanFragment.java
+++ app/src/main/java/com/skytel/sdp/ui/plan/PlanFragment.java
@@ -1,19 +1,16 @@
 package com.skytel.sdp.ui.plan;
 
 
+import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -29,10 +26,13 @@ import java.util.ArrayList;
 public class PlanFragment extends Fragment {
 
     private PieChart pieChart;
+    private BarChart barChart;
+
     private Context mContext;
 
     public PlanFragment() {
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +46,8 @@ public class PlanFragment extends Fragment {
 
         pieChart = (PieChart) rootView.findViewById(R.id.pieChart);
         setData(4, 100);
+        barChart = (BarChart) rootView.findViewById(R.id.barChart);
+        setBarData();
 
         return rootView;
     }
@@ -57,14 +59,14 @@ public class PlanFragment extends Fragment {
 
         for (int i = 0; i < count; i++) {
             values.add(new Entry((float) ((Math.random() * range) + range / 5), 10));
-            xVals.add(i+"");
+            xVals.add(i + "");
         }
 
         PieDataSet dataSet = new PieDataSet(values, "Election Results");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
 
-        PieData data = new PieData(xVals,dataSet);
+        PieData data = new PieData(xVals, dataSet);
         data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(11f);
         data.setValueTextColor(Color.WHITE);
@@ -72,5 +74,53 @@ public class PlanFragment extends Fragment {
         pieChart.invalidate();
     }
 
+    private void setBarData() {
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < 10; i++) {
+            float mult = 10;
+            float val1 = (float) (Math.random() * mult) + mult / 3;
+            float val2 = (float) (Math.random() * mult) + mult / 3;
+            float val3 = (float) (Math.random() * mult) + mult / 3;
+
+            yVals1.add(new BarEntry(new float[]{val1, val2, val3}, i));
+        }
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            xVals.add(i+"");
+        }
+
+
+        BarDataSet set1;
+
+            set1 = new BarDataSet(yVals1, "Statistics Vienna 2014");
+            set1.setColors(getColors());
+            set1.setStackLabels(new String[]{"Births", "Divorces", "Marriages"});
+
+            ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(xVals,dataSets);
+//            data.setValueFormatter(new MyValueFormatter());
+            data.setValueTextColor(Color.WHITE);
+
+            barChart.setData(data);
+
+        barChart.invalidate();
+    }
+
+    private int[] getColors() {
+
+        int stacksize = 3;
+
+        // have as many colors as stack-values per entry
+        int[] colors = new int[stacksize];
+
+        for (int i = 0; i < colors.length; i++) {
+            colors[i] = ColorTemplate.VORDIPLOM_COLORS[i];
+        }
+
+        return colors;
+    }
 
 }
